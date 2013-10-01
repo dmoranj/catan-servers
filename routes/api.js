3930d295abaf0ff9c18c18c2319e9f9860a832d3
Javier Mendiara Cañardo <javier@tid.es>
2013-10-01 20:19:21 +0200

    REFACTOR for better understanding

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,15 @@
 var rankingService = require('../lib/rankingService'),
     restTools = require("../lib/restTools");
 
+function ranking(req, res) {
+  rankingService.top(function(err, data) {
+    if (err) {
+      return res.json(500, restTools.generateError("101", err));
+    }
+    res.json(data);
+  });
+}
+
 module.exports = {
-
-  ranking: function(req, res) {
-    rankingService.top(function(err, data) {
-      if (err) {
-        return res.json(500, restTools.generateError("101", err));
-      }
-      res.json(data);
-    });
-  }
-
+  ranking: ranking
 };
